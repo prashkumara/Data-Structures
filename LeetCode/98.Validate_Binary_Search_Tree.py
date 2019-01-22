b2ed4d6dcf86223ff5bd97786a02203b20fd502d
prashkumara <prashkumar.a@gmail.com>
2019-01-22 17:43:02 +0100

    Accepted Solution

---
+++ LeetCode/98.Validate_Binary_Search_Tree.py
@@ -0,0 +1,26 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def isValidBST(self, root):
+        """
+        :type root: TreeNode
+        :rtype: bool
+        """
+        result = []
+
+        self.inorder(root, result)
+        for i in range(1, len(result)):
+            if result[i - 1] >= result[i]:
+                return False
+        return True
+
+    def inorder(self, root, result):
+        if root:
+            self.inorder(root.left, result)
+            result.append(root.val)
+            self.inorder(root.right, result)
